252a636e28bced5df5d52390a61ebf8c7fdbbfb9
Utpal <08utpal@gmail.com>
2023-07-13 06:38:05 +0530

    Done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,22 +1,31 @@
 package com.driver;
 
-public class Main {
-    public static class A {
-        public String meth() {
-            return "Invoking method from class A";
-        }
+// Step 1: Create class A with a method named meth
+class A {
+    public String meth() {
+        return "Invoking method from class A";
+    }
+}
 
-        public static class B extends A {
+// Step 2: Create class B which extends class A
+class B extends A {}
+
+public class Main {
+    public static void main(String[] args) {
+        // Step 3: Create an object of class B and call the method meth from class A
+        B objB = new B();
+        System.out.println(objB.meth());  // Output: Invoking method from class A
+
+        // Step 4: Override the method meth in class B
+        class B extends A {
+            @Override
             public String meth() {
-                return "Method is overridden in Extendend class B";
+                return "Method is overridden in Extended class B";
             }
         }
 
-        public static void main(String[] args) {
-            B obj = new B();
-            obj.meth();
-            B obj1 = new B();
-            obj1.meth();
-        }
+        // Step 5: Call the overridden method from the object of class B
+        B objB2 = new B();
+        System.out.println(objB2.meth());  // Output: Method is overridden in Extended class B
     }
 }
